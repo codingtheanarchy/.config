e338f5e507c0a63bdd3690ea891d803020e5c5f2
Honeypot <eric@thezombiecoder.com>
2023-03-12 18:10:09 -0700

    Added new theme:  everforest

--- nvim/lua/user/colorscheme.lua
+++ nvim/lua/user/colorscheme.lua
@@ -1,4 +1,6 @@
-local colorscheme = "tokyonight-night"
+-- local colorscheme = "tokyonight-night"
+local colorscheme = "everforest"
+
 
 local status_ok, _ = pcall(vim.cmd.colorscheme, colorscheme)
 if not status_ok then
